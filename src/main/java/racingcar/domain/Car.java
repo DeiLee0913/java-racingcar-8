b47dacb942de6568d1a494f6984ccf61e660e822
D <deilee0913@gmail.com>
2025-10-26 15:22:56 +0900

    feat(domain): Car Domain 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+public class Car {
+    private static final int MOVE_THRESHOLD = 4;
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move(int randomeNumber) {
+        if(randomeNumber < MOVE_THRESHOLD) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String toString() {
+        return name + " : " + "-".repeat(position);
+    }
+}
