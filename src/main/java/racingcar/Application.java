fd6b32d18fca2cb589f04a31146bc314627c4165
D <deilee0913@gmail.com>
2025-10-27 15:49:26 +0900

    feat(app): Application 실행 진입점(main) 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import racingcar.controller.CarRacingController;
+import racingcar.util.CarFactory;
+import racingcar.util.NameParser;
+import racingcar.util.NameValidator;
+import racingcar.util.TryCountValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        NameParser nameParser = new NameParser();
+        NameValidator nameValidator = new NameValidator();
+        TryCountValidator tryCountValidator = new TryCountValidator();
+        CarFactory carFactory = new CarFactory();
+
+        CarRacingController carRacingController = new CarRacingController
+                (inputView, outputView, nameParser, nameValidator, tryCountValidator, carFactory);
+
+        carRacingController.run();
     }
 }
