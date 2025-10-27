e215f39b27c86525fbd5faa53043f0fc96167c5e
D <deilee0913@gmail.com>
2025-10-27 12:00:06 +0900

    feat(factory): CarFactory 구현
    - 전달받은 이름으로 Car Instance 생성
    - List<Car>로 반환

---
+++ src/main/java/racingcar/util/CarFactory.java
@@ -0,0 +1,18 @@
+package racingcar.util;
+
+import racingcar.domain.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarFactory {
+    public List<Car> createCars(String[] names) {
+        List<Car> cars = new ArrayList<>();
+
+        for(String name : names) {
+            Car car = new Car(name);
+            cars.add(car);
+        }
+        return cars;
+    }
+}
